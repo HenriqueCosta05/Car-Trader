a59215f9da42468a6a6cb42840c1ff045a06ab87
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2024-07-31 13:42:36 -0300

    feat: added functionality in navbar and searchbar.

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,6 +2,6 @@
   <header
     class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
   >
-    <a class="text-3xl font-mono" href="">Car-Trader</a>
+    <NuxtLink class="text-3xl font-mono" to="/">Car-Trader</NuxtLink>
   </header>
 </template>
