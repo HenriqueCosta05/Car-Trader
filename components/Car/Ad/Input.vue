a8436875e0aeaadceaed97803ff00d787594cc03
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2024-08-01 16:53:19 -0300

    feat: created static Car components

---
+++ components/Car/Ad/Input.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col w-[48%] mt-2">
+    <label for="" class="text-cyan-500 mb-1 text-sm">Static input</label>
+    <input
+      type="text"
+      class="p-2 border w-100 rounded"
+      :placeholder="placeholder"
+      v-model="state"
+      :name="name"
+      @input="onChange"
+    />
+  </div>
+</template>
---
+++ components/Car/Ad/Select.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col w-[48%] mt-2">
+    <label for="" class="text-cyan-500 mb-1 text-sm">Static Select</label>
+    <select
+      name=""
+      id=""
+      class="p-2 border w-100 rounded"
+      @change="onChange"
+      v-model="state"
+    >
+      <option v-for="option in options" :key="option.id" :value="option">
+        Static Option
+      </option>
+    </select>
+  </div>
+</template>
