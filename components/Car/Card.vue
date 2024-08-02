12a2f5551d114e751dea42f3320cd0edf2341829
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2024-08-02 13:29:38 -0300

    bugfix: fixing yarn local issues

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,22 +1,42 @@
+<script setup lang='ts'>
+import heartFilled from "../../assets/heartFilled.png";
+import heartOutline from "../../assets/heartOutline.png";
+
+const props = defineProps<{
+  car: {
+    id: number;
+    name: string;
+    price: string;
+    url: string;
+    seats: number;
+    miles: string;
+    features: string[];
+    description: string;
+  };
+  favorited: boolean;
+}>();
+
+const emit = defineEmits(["favorite"]);
+</script>
+
 <template>
-    <div
-        class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
-      >
-        <div class="flex h-full">
-          <img
-            src="https://carwow-uk-wp-3.imgix.net/Volvo-XC40-white-scaled.jpg"
-            alt=""
-            class="w-[300px] h-full"
-          />
-          <div class="p-4 flex flex-col">
-            <div>
-              <h1 class="text-2xl text-blue-700">Volvo XC40</h1>
-              <p class="text-gray-700">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </p>
-            </div>
-            <h1 class="mt-auto text-xl">$39,555</h1>
-          </div>
+  <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
+    <div class="flex h-full">
+      <img
+        src="https://carwow-uk-wp-3.imgix.net/Volvo-XC40-white-scaled.jpg"
+        alt=""
+        class="w-[300px] h-full"
+      />
+      <div class="p-4 flex flex-col">
+        <div>
+          <h1 class="text-2xl text-blue-700">Volvo XC40</h1>
+          <p class="text-gray-700">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
         </div>
+        <h1 class="mt-auto text-xl">$39,555</h1>
       </div>
+    </div>
+  </div>
 </template>
+    
--- pages/city/[city]/car/[[make]].vue
+++ pages/city/[city]/car/[[make]].vue
@@ -1,3 +1,18 @@
+<<<<<<< HEAD
+=======
+<script setup lang="ts">
+import { useLocalStorage } from '@vueuse/core';
+
+const { cars } = useCars();
+
+const favorite = useLocalStorage("favorite", []);
+
+const handleFavoriteCars = (id: number) => {
+  if (JSON.stringify(id) in favorite.value) delete favorite.value[id];
+  else favorite.value = { ...favorite.value, [id]: true };
+};
+</script>
+>>>>>>> f33cb1a (bugfix: fixing yarn local issues)
 <template>
   <div>
     <CarCards />
